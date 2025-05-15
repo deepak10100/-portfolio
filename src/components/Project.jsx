cd2dbd73aa7a9025d10b40853dc35239fc9921d8
deepak10100 <deepakmalakar98185@gmail.com>
2025-05-15 10:25:42 +0530

    updates

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -2,6 +2,11 @@ import React from 'react'
 import {  GridItem, Heading, SimpleGrid, Stack,  Text } from '@chakra-ui/react'
 import ProjectCard from './ProjectCard'
 import weather from '../assets/projectimages/weather.png'
+import NoteApp from '../assets/projectimages/Noteapp.png'
+import ProductPage from '../assets/projectimages/productpage.png'
+import Todo from '../assets/projectimages/todo.png'
+import Card from '../assets/projectimages/cart.png'
+import Blog from '../assets/projectimages/blog.png'
 function Project() {
   return (
     <>
@@ -12,11 +17,25 @@ function Project() {
           <Text as={'p'}>List of projects that I involved or have made in the past.</Text>
           <SimpleGrid py={7} alignItems={'center'} justifyContent={'center'} columns={[1,null,3]} columnGap={2} rowGap={5}  >
             <GridItem color={'red'}>
-            <ProjectCard image={weather} blogUrl={'https://weather-app-six-green-76.vercel.app/'} title={'Weather Site'} desc={'Here is a weather site which gives us weather status. Here is a simple weather site which gives some general weather details.'} bedge1={'vite+react'} bedge2={'chakraui'}  bedge3={'axios'} />
+            <ProjectCard image={weather} blogUrl={'https://weather-app-six-green-76.vercel.app/'} title={'Weather Site'} bedge1={'vite+react'} bedge2={'chakraui'}  bedge3={'axios'} />
+            </GridItem>
+            <GridItem color={'red'}>
+            <ProjectCard image={NoteApp} blogUrl={'https://deepak10100.github.io/my-website/Note.html'} title={"Note's Site"}  bedge1={'javascript'} bedge2={'CSS'}  bedge3={'HTML'} />
+            </GridItem>
+            <GridItem color={'red'}>
+            <ProjectCard image={ProductPage} blogUrl={'https://deepak10100.github.io/my-website/ProductPage.html'} title={"Product Page"}  bedge1={'Tailwind Css'}   bedge3={'HTML'} />
+            </GridItem>
+            <GridItem color={'red'}>
+            <ProjectCard image={Todo} blogUrl={'https://deepak10100.github.io/my-website/todo.html'} title={"Todo App"}  bedge1={'HTML'} bedge2={'CSS'}  bedge3={'Javascript'} />
+            </GridItem>
+            <GridItem color={'red'}>
+            <ProjectCard image={Card} blogUrl={'https://deepak10100.github.io/my-website/newCard.html'} title={"Card"} bedge2={'CSS'}  bedge3={'HTML'} />
+            </GridItem>
+            <GridItem color={'red'}>
+            <ProjectCard image={Blog} blogUrl={'https://deepak10100.github.io/my-website/blog.html'} title={"Blog Page"}  bedge1={'tailwind css'}  bedge3={'HTML'} />
             </GridItem>
             
-            
-            
+    
           </SimpleGrid>
       </Stack>
 
--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -7,23 +7,17 @@ function ProjectCard({title,desc, image,bedge1,bedge2,bedge3,bedge4,blogUrl}) {
     <>
      <Card maxW={'sm'}  cursor={"pointer"} _hover={{ borderWidth:"1px",borderColor:"#39FF14"}}>
      <Link to={blogUrl}>
-                <CardBody>
-                <Image
-      src={image} width={'full'} height={'200px'}
-      alt='Green double couch with wooden legs'
-      borderRadius='lg'
-    />
+      <CardBody>
+      <Image src={image} width={'full'} height={'200px'} alt='Green double couch with wooden legs' borderRadius='lg'/>
     <CardHeader px={0}>
-        <Heading as={'h1'} fontSize={[null,'large','large']}>{title} </Heading>
+    <Heading as={'h1'} fontSize={[null,'large','large']}>{title} </Heading>
     </CardHeader>
     <Text as={'p'} >{desc}</Text>
-                </CardBody>
-                <CardFooter>
-                
-                {/* <Stack direction={['row','column','row']} width={'sm'}> */}
+    </CardBody>
+    
+  <CardFooter>
+                         
     <SimpleGrid columns={['3','4']} gap={4} >
-
-   
  <GridItem colSpan={1}>
  <Badge as={'p'} borderRadius={7} fontSize={'x-small'} colorScheme='purple'>{bedge1}</Badge>
  </GridItem>
@@ -36,11 +30,14 @@ function ProjectCard({title,desc, image,bedge1,bedge2,bedge3,bedge4,blogUrl}) {
  <GridItem colSpan={1}>
  <Badge as={'p'} borderRadius={7} fontSize={'x-small'} colorScheme='purple'>{bedge4}</Badge>
  </GridItem>
-{/* </Stack> */}
 </SimpleGrid>
 </CardFooter>
      </Link>
+
+     
       </Card>
+
+      
     </>
   )
 }
